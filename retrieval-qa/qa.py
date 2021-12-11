8b54a23085a7b78527aea2c5dac22233fbcd1863
habanera <ffflfffl@126.com>
2021-12-11 20:04:29 +0800

    加testcase

--- retrieval-qa/qa.py
+++ retrieval-qa/qa.py
@@ -1,5 +1,7 @@
 # 似乎这样用相对导入也没bug
-from .. import retriever
+import time
+
+import retriever
 from reader import Reader
 
 
@@ -27,8 +29,36 @@ class QA:
 
         return outputs[0]['answer']
 
+    def simple_ans(self, query):
+        start_time = time.process_time()
+        doc_id = self.ranker.closest_docs(query, k=1)[0][0]
+        document_text = self.database.get_doc_text(doc_id)
+        outputs = self.reader.answer(query, document_text)
+        # print('time cost:', time.process_time()-start_time)
+        return outputs
+
 
 if __name__ == '__main__':
     qa_system = QA()
-    ans = qa_system('北理工的校长是谁？')
-    print(ans)
+    questions = ['天安门在哪？',
+                 '习近平在哪里出生？',
+                 '贝多芬于哪里出生？',
+                 '鲲鹏处理器是什么？',
+                 '猫有几条腿？',
+                 '为什么我复习不完？',
+                 '牛顿三定律是什么？',
+                 '1453年发生了什么？',
+                 '苹果手机是哪一年发布的？',
+                 '什么是苹果手机？',
+                 '什么是中央处理器？',
+                 '摩尔定律是什么？',
+                 '什么是人工智能？',
+                 '什么是哥德巴赫猜想？',
+                 '什么是有限状态机?',
+                 '什么是黎曼猜想?',
+                 '刘华强是谁？',
+                 '蜜雪冰城是什么?']
+
+    for i, q in enumerate(questions):
+        print('问题', i, ':', q)
+        print('    ', qa_system.simple_ans(q))
